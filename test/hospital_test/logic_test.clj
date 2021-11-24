ec65ab9ae7b90cc9f09d6829c75bd7448e4a6deb
David Borelli <david_borelli@live.com>
2021-11-23 21:20:32 -0300

    extracting variables

--- test/hospital_test/logic_test.clj
+++ test/hospital_test/logic_test.clj
@@ -32,46 +32,48 @@
 
 
 (deftest chega-em-test
-  (testing "aceita pessoas enquanto cabem pessoas na fila"
-    ;(is (= {:espera [1 2 3 4 5]}
-    ;       (chega-em {:espera [1 2 3 4]}, :espera, 5)))
-    ;
-    ;(is (= {:espera [1 2 5]}
-    ;       (chega-em {:espera [1 2]}, :espera, 5)))
+  (let [hospital-cheio {:espera [1 21 32 23 43]}]
 
-    (is (= {:hospital {:espera [1 2 3 4 5]}, :resultado :sucesso}
-           (chega-em {:espera [1 2 3 4]}, :espera, 5)))
+    (testing "aceita pessoas enquanto cabem pessoas na fila"
+      ;(is (= {:espera [1 2 3 4 5]}
+      ;       (chega-em {:espera [1 2 3 4]}, :espera, 5)))
+      ;
+      ;(is (= {:espera [1 2 5]}
+      ;       (chega-em {:espera [1 2]}, :espera, 5)))
 
-    (is (= {:hospital {:espera [1 2 5]}, :resultado :sucesso}
-           (chega-em {:espera [1 2]}, :espera, 5)))
+      (is (= {:hospital {:espera [1 2 3 4 5]}, :resultado :sucesso}
+             (chega-em {:espera [1 2 3 4]}, :espera, 5)))
 
-    )
+      (is (= {:hospital {:espera [1 2 5]}, :resultado :sucesso}
+             (chega-em {:espera [1 2]}, :espera, 5)))
 
-  (testing "não aceita quando não cabe na fila"
-    ; Verificando que uma exception foi jogada
-    ; código clássico ruim, usamos uma exception GENERICA.
-    ; que qualquer outro erro genérico irá lançar a mesma exception
-    ; podendo induzir ao erro, quando vamos acreditar que deu certo, mas deu errado
-    ; (is (thrown? clojure.lang.ExceptionInfo (chega-em {:espera [1 23 45 43 12]}, :espera 76)))))
+      )
 
-    ;(is (thrown? IllegalStateException
-    ;             (chega-em {:espera [1 35 42 64 21]}, :espera 76)))
+    (testing "não aceita quando não cabe na fila"
+      ; Verificando que uma exception foi jogada
+      ; código clássico ruim, usamos uma exception GENERICA.
+      ; que qualquer outro erro genérico irá lançar a mesma exception
+      ; podendo induzir ao erro, quando vamos acreditar que deu certo, mas deu errado
+      ; (is (thrown? clojure.lang.ExceptionInfo (chega-em {:espera [1 23 45 43 12]}, :espera 76)))))
 
-    ; outra abordagem, do nil
-    ; mas o perigo do swap, teriamos que trabalhar em outro ponto a condição de erro
-    ;(is (nil? (chega-em {:espera [1 21 32 23 43]}, :espera 98)))
+      ;(is (thrown? IllegalStateException
+      ;             (chega-em {:espera [1 35 42 64 21]}, :espera 76)))
 
-    ; outra maneira de testar, onde ao invés de como Java, utilizar o tipo da
-    ; exception para entender o TIPO de erro que ocorreu, estamos usando os dados
-    ; da exception para isso. Menos sensível que a mensagem de erro
-    ; mas ainda eh uma validação trabalhosa
-    ;(is (try
-    ;      (chega-em {:espera [12 13 14 19 43]}, :espera 44)
-    ;      false
-    ;      (catch clojure.lang.ExceptionInfo e
-    ;        (= :impossivel-colocar-pessoa-na-fila (:tipo (ex-data e)))
-    ;        )))
+      ; outra abordagem, do nil
+      ; mas o perigo do swap, teriamos que trabalhar em outro ponto a condição de erro
+      ;(is (nil? (chega-em {:espera [1 21 32 23 43]}, :espera 98)))
 
-    (is (= {:hospital {:espera [1 21 32 23 43]}, :resultado :impossivel-colocar-pessoa-na-fila}
-           (chega-em {:espera [1 21 32 23 43]}, :espera 98)))
-    ))
+      ; outra maneira de testar, onde ao invés de como Java, utilizar o tipo da
+      ; exception para entender o TIPO de erro que ocorreu, estamos usando os dados
+      ; da exception para isso. Menos sensível que a mensagem de erro
+      ; mas ainda eh uma validação trabalhosa
+      ;(is (try
+      ;      (chega-em {:espera [12 13 14 19 43]}, :espera 44)
+      ;      false
+      ;      (catch clojure.lang.ExceptionInfo e
+      ;        (= :impossivel-colocar-pessoa-na-fila (:tipo (ex-data e)))
+      ;        )))
+
+      (is (= {:hospital hospital-cheio, :resultado :impossivel-colocar-pessoa-na-fila}
+             (chega-em hospital-cheio, :espera 98)))
+      )))
